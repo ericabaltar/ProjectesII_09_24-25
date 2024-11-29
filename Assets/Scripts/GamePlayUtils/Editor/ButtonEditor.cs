55202e6402a319916088c992909027f79be9b131
Edgar Novas <edgar.novas@enti.cat>
2024-11-29 15:38:23 +0100

    Button Utils

---
+++ Assets/Scripts/GamePlayUtils/Editor/ButtonEditor.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(ButtonGameplay))]
+public class ButtonEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+    }
+}
