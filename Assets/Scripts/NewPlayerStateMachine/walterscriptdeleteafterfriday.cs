29ca6a82420154a470add08e6a6fe4c2fe6d94c3
WalterScherk <walter.scherk@enti.cat>
2024-11-29 10:33:00 +0100

    Load half of levels

--- Assets/Scripts/NewPlayerStateMachine/walterscriptdeleteafterfriday.cs
+++ Assets/Scripts/NewPlayerStateMachine/walterscriptdeleteafterfriday.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class walterscriptdeleteafterfriday : MonoBehaviour
 {
@@ -17,13 +18,11 @@ public class walterscriptdeleteafterfriday : MonoBehaviour
         if (Input.GetKey(KeyCode.D))
         {
             anim.SetBool("walking",true);
-            Debug.Log(anim.GetBool("walking"));
             GetComponent<SpriteRenderer>().flipX = false;
         }
         else if (Input.GetKey(KeyCode.A))
         {
             anim.SetBool("walking",true);
-            Debug.Log(anim.GetBool("walking"));
             GetComponent<SpriteRenderer>().flipX = true;
         }
         else
@@ -32,8 +31,21 @@ public class walterscriptdeleteafterfriday : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Q)) {
             transform.Rotate(Vector3.forward * -90);
         }
-        if (Input.GetKeyDown(KeyCode.E)) {
+        else if (Input.GetKeyDown(KeyCode.E)) {
             transform.Rotate(Vector3.forward * 90);
         }
+
+        if (Input.GetKeyDown(KeyCode.Alpha0))
+            SceneManager.LoadScene(1);
+        else if (Input.GetKeyDown(KeyCode.Alpha1))
+            SceneManager.LoadScene(2);
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+            SceneManager.LoadScene(3);
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+            SceneManager.LoadScene(4);
+        else if (Input.GetKeyDown(KeyCode.Alpha4))
+            SceneManager.LoadScene(5);
+        else if (Input.GetKeyDown(KeyCode.Alpha5))
+            SceneManager.LoadScene(6);
     }
 }
