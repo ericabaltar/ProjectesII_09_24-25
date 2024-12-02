70b40d04ddb3b509272028e03fb654d27c1aeca7
WalterScherk <walter.scherk@enti.cat>
2024-12-02 10:53:04 +0100

    Corrected animation sprite and -+ level swapping

--- Assets/Scripts/NewPlayerStateMachine/walterscriptdeleteafterfriday.cs
+++ Assets/Scripts/NewPlayerStateMachine/walterscriptdeleteafterfriday.cs
@@ -7,6 +7,16 @@ public class walterscriptdeleteafterfriday : MonoBehaviour
 {
     private Animator anim;
     private bool canRotate = true;
+
+    private float initialStretchValue = 1.0f;
+    private float maximumStretchValue = 1.0f;
+
+    [SerializeField] private AnimationCurve squashAndStretchCurve = new AnimationCurve(
+        new Keyframe(0, 0.0f),
+        new Keyframe(0.25f, 1.0f),
+        new Keyframe(1.0f, 0.0f)
+        )
+        ;
     void Start()
     {
         anim = gameObject.GetComponent<Animator>();
@@ -54,25 +64,10 @@ public class walterscriptdeleteafterfriday : MonoBehaviour
         }
 
         if (Input.GetKeyDown(KeyCode.Alpha0))
-            SceneManager.LoadScene(1);
-        else if (Input.GetKeyDown(KeyCode.Alpha1))
-            SceneManager.LoadScene(2);
-        else if (Input.GetKeyDown(KeyCode.Alpha2))
-            SceneManager.LoadScene(3);
-        else if (Input.GetKeyDown(KeyCode.Alpha3))
-            SceneManager.LoadScene(4);
-        else if (Input.GetKeyDown(KeyCode.Alpha4))
-            SceneManager.LoadScene(5);
-        else if (Input.GetKeyDown(KeyCode.Alpha5))
-            SceneManager.LoadScene(6);
-        else if (Input.GetKeyDown(KeyCode.Alpha6))
-            SceneManager.LoadScene(7);
-        else if (Input.GetKeyDown(KeyCode.Alpha7))
-            SceneManager.LoadScene(8);
-        else if (Input.GetKeyDown(KeyCode.Alpha8))
-            SceneManager.LoadScene(9);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         else if (Input.GetKeyDown(KeyCode.Alpha9))
-            SceneManager.LoadScene(10);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        
     }
 
     IEnumerator WaitForRotation() {
