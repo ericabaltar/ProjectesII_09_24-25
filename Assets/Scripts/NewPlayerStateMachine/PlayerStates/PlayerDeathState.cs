e43da4506e7c20965e2b9df44049267d05161a2b
Edgar Novas <edgar.novas@enti.cat>
2024-12-03 14:57:58 +0100

    Death Finished

--- Assets/Scripts/NewPlayerStateMachine/PlayerStates/PlayerDeathState.cs
+++ Assets/Scripts/NewPlayerStateMachine/PlayerStates/PlayerDeathState.cs
@@ -1,20 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerDeathState : PlayerBaseState
 {
     public PlayerDeathState(PlayerStateMachine stateMachine) : base(stateMachine)
     {
     }
-
+    float timeToRestart = 2f;
+    float pushUp = 20f;
+    float pushToSide = 20f;
     public override void Enter()
     {
-        
+        stateMachine.particlesDeath.Play();
+        stateMachine.GetComponent<Collider2D>().enabled = false;
+        int direction = Random.Range(0, 2);
+        Debug.Log(direction);
+        stateMachine.rigidbody2d.AddForce(new Vector2(direction == 0 ? pushToSide : -pushToSide, pushUp), ForceMode2D.Impulse);
     }
 
     public override void Tick(float deltaTime)
     {
+        timeToRestart -= Time.deltaTime;
+        stateMachine.transform.Rotate(Vector3.forward, 340f * deltaTime);
+        if(timeToRestart <= 0f) 
+        {
+            stateMachine.RestartLevel();
+        }
         //Death
     }
 
