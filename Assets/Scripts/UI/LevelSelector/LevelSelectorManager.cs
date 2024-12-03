cabd6f5fb2e31f9a2df8fa48aec7756f7208763b
EdgarNovas <edgar.novas@enti.cat>
2024-12-03 23:58:23 +0100

    Level Selector Done

---
+++ Assets/Scripts/UI/LevelSelector/LevelSelectorManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelSelectorManager : MonoBehaviour
+{
+    //List<Button> levels = new List<Button>();
+    
+    public void LoadSelectedLevel(int level)
+    {
+        SceneManager.LoadScene(level);
+    }
+}
