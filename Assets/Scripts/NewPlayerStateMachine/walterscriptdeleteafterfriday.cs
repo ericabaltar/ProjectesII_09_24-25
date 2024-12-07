1409ef69dae0a5f6f3d3d1bc39bcabfb7ea987dc
WalterScherk <walter.scherk@enti.cat>
2024-12-07 14:04:56 +0100

    Deletion of useless code and addition of sound when walking

--- Assets/Scripts/NewPlayerStateMachine/walterscriptdeleteafterfriday.cs
+++ Assets/Scripts/NewPlayerStateMachine/walterscriptdeleteafterfriday.cs
@@ -12,11 +12,13 @@ public class walterscriptdeleteafterfriday : MonoBehaviour
     private bool canSquish = false;
     PlayerStateMachine playerState;
     SpriteRenderer mySprite;
+    AudioSource myAudioSource;
     void Start()
     {
         anim = gameObject.GetComponentInChildren<Animator>();
         mySprite = gameObject.GetComponentInChildren<SpriteRenderer>();
         playerState = gameObject.GetComponentInParent<PlayerStateMachine>();
+        myAudioSource = GetComponent<AudioSource>();
 
         ConstraintSource source = new ConstraintSource();
         source.sourceTransform = GameObject.Find("Main Camera").transform;
@@ -31,51 +33,34 @@ public class walterscriptdeleteafterfriday : MonoBehaviour
         {
             anim.SetBool("walking",true);
             mySprite.flipX = false;
+            if (!myAudioSource.isPlaying && playerState.isGrounded)
+                myAudioSource.Play();
         }
         else if (Input.GetKey(KeyCode.A))
         {
             anim.SetBool("walking",true);
             mySprite.flipX = true;
+            if (!myAudioSource.isPlaying && playerState.isGrounded)
+                myAudioSource.Play();
         }
-        else if (Input.GetKey(KeyCode.F))
-        {
-            canSquish = true;
-            canStretch = false;
-            anim.SetBool("falling",true);
-        }
-        else if (Input.GetKey(KeyCode.G))
-        {            
-            anim.SetBool("falling", false);
-        }
-        else
+        else { 
             anim.SetBool("walking",false);
-
-        if (Input.GetKeyDown(KeyCode.Q)) {
-            if (playerState.isWallWalking || playerState.isGrounded) {
-                if (canRotate == true)
-                {
-                    StartCoroutine(WaitForStretch());
-                }
-            }
-                
+            if (myAudioSource.isPlaying)
+                myAudioSource.loop = false;
         }
-        else if (Input.GetKeyDown(KeyCode.E)) {
-            if (playerState.isWallWalking || playerState.isGrounded) {
 
-                if (canRotate == true)
-                {
-                    StartCoroutine(WaitForStretch());
-                }
-            }
-                
-        }
 
         if (!playerState.isGrounded && !playerState.isWallWalking)
         {
             anim.SetBool("falling", true);
         }
+        else if (anim.GetBool("falling") && playerState.isGrounded) { 
+            anim.SetBool("falling", false);
+            myAudioSource.Play();
+        }
         else
             anim.SetBool("falling", false);
+
 
         if (Input.GetKeyDown(KeyCode.Alpha0))
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -84,18 +69,5 @@ public class walterscriptdeleteafterfriday : MonoBehaviour
     }
 
     
-    IEnumerator WaitForStretch() {
-        canRotate = true;
-        yield return new WaitForSeconds(1.0f);
-        anim.SetBool("falling", true);
-
-        canStretch = false;
-    }
-
-    IEnumerator Land() {
-        anim.SetBool("faling", false);
-
-        yield return new WaitForSeconds(0.1f);
-
-    }
+    
 }
