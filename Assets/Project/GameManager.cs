3e1f84dee6e70ea11a4474238c5a3751b1521369
Edgar Novas <edgar.novas@enti.cat>
2024-12-13 19:48:25 +0100

    Map goes brrr HELP ME

--- Assets/Project/GameManager.cs
+++ Assets/Project/GameManager.cs
@@ -2,18 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using static UnityEngine.Rendering.DebugUI.Table;
 
 public class GameManager : MonoBehaviour
 {
+    public enum RotationState { IDLE, ROTATING, ADJUSTING }
+
+    public RotationState rotationState { get; private set; } = RotationState.IDLE;
+
     public static GameManager Instance;
 
     public UnityEvent rotationFinishEvent;
 
-    public AnimationCurve rotationCurve;
-    public const float RotationAngle = 90.0f;
-    public float rotationTime = 2.0f;
-    private float currentRotationTime = 0.0f;
+    public float[] PossibleRotations;
+    private int targetRotation = 0; //index of PossibleRotations
+
+    public const float RotationAngle = 90.0f; //Deg
+    public const float RotationSpeed = 90.0f; //Deg/s
     private float lastAngle = 0.0f;
+
+    public AnimationCurve rotationCurve;
+    private float rotationTime = 0.0f;
+    private float currentRotationTime = 0.0f;
+    private float remainingRotation = 0.0f;
 
     public List<GameObject> objectsToConsider; // Assign in the inspector
     private Vector3 centerPoint;
@@ -29,43 +40,87 @@ public class GameManager : MonoBehaviour
 
         Instance = this;
     }
+
+    private void RotatingStateUpdate()
+    {
+        float rotationAngle = lastAngle + Time.deltaTime;
+        lastAngle = rotationAngle;
+        RotateObjectsInScene(rotationAngle);
+        //If key is released stop rotation
+        //If degrees are fine stop rotation
+    }
+
+    private void AdjustingStateUpdate()
+    {
+        currentRotationTime = Mathf.Min(currentRotationTime + Time.deltaTime, rotationTime);
+        float t = currentRotationTime / rotationTime;
+
+        float rot = rotationCurve.Evaluate(t) * (rotatingRight ? -1.0f : 1.0f);
+        rot *= remainingRotation;
+
+        //update rotation
+        float rotationAngle = rot - lastAngle;
+        lastAngle = rot;
+        RotateObjectsInScene(rotationAngle);
+
+        if (t == 1.0f)
+        {
+            StopRotation();
+            //Rotation completed
+            StartCoroutine(WaitFixedUpdateAndEnableRigidbodies());
+            rotationFinishEvent.Invoke();
+            rotationState = RotationState.IDLE;
+        }
+    }
     void Update()
     {
-        if (isRotating)
-        { //Update rotation
-            currentRotationTime = Mathf.Min(currentRotationTime + Time.deltaTime, rotationTime);
-            float t = currentRotationTime / rotationTime;
-
-            float rot = rotationCurve.Evaluate(t) * (rotatingRight ? -1.0f : 1.0f);
-            rot *= RotationAngle;
-
-            //update rotation
-            float rotationAngle = rot - lastAngle;
-            lastAngle = rot;
-            RotateObjectsInScene(rotationAngle);
-
-            if (t == 1.0f)
-            {
-                //Rotation completed
-                StartCoroutine(WaitFixedUpdateAndEnableRigidbodies());
-                rotationFinishEvent.Invoke();
-                isRotating = false;
-            }
+        switch (rotationState)
+        {
+            case RotationState.ROTATING:
+                RotatingStateUpdate();
+                break;
+            case RotationState.ADJUSTING:
+                AdjustingStateUpdate();
+                break;
         }
     }
 
     public void StartRotation(bool goesRight)
     {
-        if(!isRotating)
-        {
-            isRotating = true;
-            rotatingRight = goesRight;
-            PlayRotationSound();
-            lastAngle = 0;
-            currentRotationTime = 0.0f;
-            ToggleRigidbodiesInScene(false);
+        if (rotationState != RotationState.IDLE)
+            return;
+
+        rotationState = RotationState.ROTATING;
+        rotatingRight = goesRight;
+        PlayRotationSound();
+        lastAngle = 0;
+        currentRotationTime = 0.0f;
+        ToggleRigidbodiesInScene(false);
+    }
+
+    public void StopRotation()
+    {
+        if (rotationState != RotationState.ROTATING)
+            return;
+
+        //Calculate the closest valid angle
+        targetRotation = 0;
+        float currentAngle = transform.rotation.eulerAngles.z;
+        remainingRotation = (360f + PossibleRotations[targetRotation]) - (360f + currentAngle);
+        for(int i = 1; i < PossibleRotations.Length; i++) {
+            float dist = (360f + PossibleRotations[i]) - (360f + currentAngle);
+            if (dist < remainingRotation)
+            {
+                targetRotation = i;
+                remainingRotation = dist;
+            }
         }
-        
+
+        rotationState = RotationState.ADJUSTING;
+
+        float breakingFactor = 1f; //Smooths the ending
+        rotationTime = (remainingRotation * breakingFactor) / RotationSpeed;
+        currentRotationTime = 0.0f;
     }
 
     void RotateObjectsInScene(float angle)
