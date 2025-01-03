68fc25a802c07652f94487e1c093bbf264ac67a8
erica <erica.baltar@enti.cat>
2025-01-03 22:43:12 +0100

    Button Functionality Done

--- Assets/Scripts/GamePlayUtils/ButtonGameplay.cs
+++ Assets/Scripts/GamePlayUtils/ButtonGameplay.cs
@@ -4,44 +4,53 @@ using UnityEngine;
 
 public class ButtonGameplay : MonoBehaviour
 {
-    #region variables
-    public enum ButtonDoings
+
+    [SerializeField] private GameObject door;
+
+    private int pressCount = 0;
+
+    private void OnTriggerEnter(Collider other)
     {
-        None,
-        Disappear,
-        Move,
-        Timer
+
+        if (other.CompareTag("Player") || other.CompareTag("Box"))
+        {
+            pressCount++;
+            if (pressCount == 1) 
+            {
+                OnButtonPressed();
+            }
+        }
     }
 
-    public ButtonDoings buttonTypes;
-
-    public GameObject objectToEdit;
-
-    [SerializeField] float timer = 0f;
-    [SerializeField] float timerReset = 50f;
-    [SerializeField] float timeToMove = 40f;
-    #endregion
-
-
-    public void OnPressedButton()
+    private void OnTriggerExit(Collider other)
     {
-        switch(buttonTypes)
+       
+        if (other.CompareTag("Player") || other.CompareTag("Box"))
         {
-            case ButtonDoings.None:
-                break; 
-            case ButtonDoings.Disappear:
-                objectToEdit.SetActive(false);
-                break;
-            case ButtonDoings.Move:
-                
-                break;
-            case ButtonDoings.Timer:
-                objectToEdit.SetActive(false);
-                break;
-            default:
-                break;
+            pressCount = Mathf.Max(0, pressCount - 1);
+            if (pressCount == 0) 
+            {
+                OnButtonReleased();
+            }
         }
+    }
 
+    private void OnButtonPressed()
+    {
+        
+        if (door != null)
+        {
+            door.SetActive(true);
+        }
+    }
 
+    private void OnButtonReleased()
+    {
+      
+        if (door != null)
+        {
+            door.SetActive(false);
+        }
     }
 }
+
--- Assets/Scripts/GamePlayUtils/Editor/ButtonEditor.cs
+++ Assets/Scripts/GamePlayUtils/Editor/ButtonEditor.cs
@@ -8,47 +8,21 @@ using UnityEngine;
 public class ButtonEditor : Editor
 {
     #region SerializeProperty
-
-    SerializedProperty buttonType;
-    SerializedProperty objectToEdit;
-    SerializedProperty timer;
-    SerializedProperty timerReset;
-    SerializedProperty timeToMove;
-
+    SerializedProperty door; 
     #endregion
 
     private void OnEnable()
     {
-        buttonType = serializedObject.FindProperty("buttonTypes");
-        objectToEdit = serializedObject.FindProperty("objectToEdit");
-        timer = serializedObject.FindProperty("timer");
-        timerReset = serializedObject.FindProperty("timerReset");
-        timeToMove = serializedObject.FindProperty("timeToMove");
-
+        door = serializedObject.FindProperty("door");
     }
+
     public override void OnInspectorGUI()
     {
         serializedObject.Update();
-        EditorGUILayout.PropertyField(buttonType);
-        if(buttonType.enumValueIndex != 0)
-        {
-            EditorGUILayout.PropertyField(objectToEdit);
-        }
-        
-        if(buttonType.enumValueIndex == 2)
-        {
-            EditorGUILayout.PropertyField(timeToMove);
-        }
 
-        if (buttonType.enumValueIndex == 3 )
-        {
-            EditorGUILayout.PropertyField (timer);
-            EditorGUILayout.PropertyField (timerReset);
+        EditorGUILayout.PropertyField(door, new GUIContent("Door"));
 
-        }
-
-
-        
         serializedObject.ApplyModifiedProperties();
     }
 }
+
