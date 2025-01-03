b30acd9d9ba7c8e5afeae58770cde8ed25fc1da9
EdgarNovas <edgar.novas@enti.cat>
2025-01-03 11:15:29 +0100

    Transition Completed and fixed

--- Assets/Scripts/TransitionFace.cs
+++ Assets/Scripts/TransitionFace.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class TransitionFace : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
+        OnStartScene();
+    }
+
+
+
+    public void OnStartScene()
+    {
+        transform.DOScale(0, 2f);
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnEndScene()
     {
-        
+        transform.DOScale(50, 2f);
     }
+
 }
