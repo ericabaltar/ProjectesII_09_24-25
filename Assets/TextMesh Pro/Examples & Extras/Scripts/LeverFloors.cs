483b787e67d14fff60c4389ad7a705716b0de45a
WalterScherk <walter.scherk@enti.cat>
2025-01-15 12:51:48 +0100

    trasspassable-floors-edit color and propper behaviour

--- Assets/TextMesh Pro/Examples & Extras/Scripts/LeverFloors.cs
+++ Assets/TextMesh Pro/Examples & Extras/Scripts/LeverFloors.cs
@@ -31,33 +31,33 @@ public class LeverFloors : MonoBehaviour
                 if (child.GetComponent<BoxCollider2D>().enabled == false)
                 {
                     child.GetComponent<SpriteRenderer>().color = new Color(
-                            1.0f
-                            , 0
-                            , 0
+                            child.GetComponent<SpriteRenderer>().color.r
+                            , child.GetComponent<SpriteRenderer>().color.g
+                            , child.GetComponent<SpriteRenderer>().color.b
                             , 1.0f);
 
                     child.GetComponent<BoxCollider2D>().enabled = true;
 
                     tiles.color = new Color(
-                            1.0f
-                            , 0
-                            , 0
+                            child.GetComponent<SpriteRenderer>().color.r
+                            , child.GetComponent<SpriteRenderer>().color.g
+                            , child.GetComponent<SpriteRenderer>().color.b
                             , 1.0f);
                 }
                 else
                 {                    
                     child.GetComponent<SpriteRenderer>().color = new Color(
-                                1.0f
-                                , 0
-                                , 0
-                                , 60.0f / 255.0f);
+                            child.GetComponent<SpriteRenderer>().color.r
+                            , child.GetComponent<SpriteRenderer>().color.g
+                            , child.GetComponent<SpriteRenderer>().color.b
+                            , 60.0f / 255.0f);
 
                     child.GetComponent<BoxCollider2D>().enabled = false;
 
                     tiles.color = new Color(
-                            1.0f
-                            , 0
-                            , 0
+                            child.GetComponent<SpriteRenderer>().color.r
+                            , child.GetComponent<SpriteRenderer>().color.g
+                            , child.GetComponent<SpriteRenderer>().color.b
                             , 60.0f / 255.0f);
                 }
             }            
