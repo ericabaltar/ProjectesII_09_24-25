ff595e0466f9b85504dc655d56e4783fb5d32c81
WalterScherk <walter.scherk@enti.cat>
2025-02-21 15:17:04 +0100

    memory leak solution

--- Assets/TextMesh Pro/Examples & Extras/Scripts/LeverFloors.cs
+++ Assets/TextMesh Pro/Examples & Extras/Scripts/LeverFloors.cs
@@ -77,15 +77,23 @@ public class LeverFloors : MonoBehaviour
         AudioSource newSource = gameObject.AddComponent<AudioSource>();
         newSource.clip = Resources.Load<AudioClip>("lever");
         newSource.PlayOneShot(newSource.clip);
+
+        Destroy(newSource, newSource.clip.length);
     }
     void PlayWallAppear() {
         AudioSource newSource2 = gameObject.AddComponent<AudioSource>();
         newSource2.clip = Resources.Load<AudioClip>("wallAppearSound");
         newSource2.PlayOneShot(newSource2.clip);
+        
+        Destroy(newSource2, newSource2.clip.length);
     }
     void PlayWallDissappear() {
         AudioSource newSource3 = gameObject.AddComponent<AudioSource>();
         newSource3.clip = Resources.Load<AudioClip>("wallDissappearSound");
         newSource3.PlayOneShot(newSource3.clip);
+
+        Destroy(newSource3, newSource3.clip.length);
     }
+
+
 }
