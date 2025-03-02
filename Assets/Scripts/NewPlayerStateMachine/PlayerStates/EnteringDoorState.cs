78be6989a18de8bf968ae9bb44afb7daff6a74ed
EdgarNovas <edgar.novas@enti.cat>
2025-03-02 23:24:45 +0100

    Constraint Enable False and rotates properly on enter

--- Assets/Scripts/NewPlayerStateMachine/PlayerStates/EnteringDoorState.cs
+++ Assets/Scripts/NewPlayerStateMachine/PlayerStates/EnteringDoorState.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using UnityEngine.Animations;
 
 public class EnteringDoorState : PlayerBaseState
 {
@@ -12,11 +13,13 @@ public class EnteringDoorState : PlayerBaseState
     
     }
     Vector3 transformMover;
-    private float lerpProgress = 0f;
+    private float lerpProgress = 2f;
     public override void Enter()
     {
+        stateMachine.rigidbody2d.velocity = Vector3.zero;
+        stateMachine.GetComponentInChildren<RotationConstraint>().enabled = false;
         stateMachine.anim.SetBool("walking", false);
-        //stateMachine.rigidbody2d.velocity = Vector3.zero;
+       
         stateMachine.GetComponent<Collider2D>().enabled = false;
         stateMachine.rigidbody2d.constraints = RigidbodyConstraints2D.None;
         stateMachine.rigidbody2d.freezeRotation = false;
@@ -24,15 +27,20 @@ public class EnteringDoorState : PlayerBaseState
         stateMachine.rigidbody2d.angularDrag = 0.1f;
         stateMachine.rigidbody2d.drag = 0.1f;
         transformMover = stateMachine.transform.position;
+        stateMachine.transform.DOMove(stateMachine.door.position, lerpProgress).SetEase(Ease.InSine);
+        //stateMachine.rigidbody2d.
     }
 
     public override void Tick(float deltaTime)
     {
+       
         if (lerpProgress < 1f) // Ensure it stops at the door
         {
             lerpProgress += deltaTime; // Increase progress
-            stateMachine.transform.position = Vector3.Lerp(transformMover, stateMachine.door.position, lerpProgress);
+            
         }
+
+        
     }
 
     public override void FixedTick()
